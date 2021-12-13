38d7ff4ba16d079bd0d3cb65226b8c8a925e0059
tao.liu <tao.liu@nvr.cn>
2021-12-13 10:22:55 +0800

    2021-12-13

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,6 @@ static void btn_event_cb(lv_event_t * e)
         lv_obj_t * label = lv_obj_get_child(btn, 0);
         lv_label_set_text_fmt(label, "Button: %d", cnt);
     }
-
-    Serial.print(code);
-    Serial.println(", btn_event_cb......");
 }
 
 /**
@@ -32,12 +29,12 @@ static void btn_event_cb(lv_event_t * e)
  */
 void lv_example_get_started_1(void)
 {
-    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());           /*Add a button the current screen*/
     lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
     lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
     lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
 
-    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_obj_t * label = lv_label_create(btn);                /*Add a label to the button*/
     lv_label_set_text(label, "Button");                     /*Set the labels text*/
     lv_obj_center(label);
 
@@ -45,6 +42,8 @@ void lv_example_get_started_1(void)
     lv_group_add_obj(group, btn);
     lv_indev_set_group(indev_encoder, group);
 }
+
+
 /**
  * @brief  初始化接口
  */
@@ -96,8 +95,7 @@ void setup() {
  */
 void loop() {
     HAL::Update();
-
-    delay(500);
+    delay(20);
 }
 #endif
 
